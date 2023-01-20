0d1d4bbfc6550219934ab84aba4faea5decab17d
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-20 13:08:30 +0530

    commit

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React,{useRef} from 'react';
 
 export default (props) => {
-;
+const ref = useRef();
 
   return (
     <div>
-          <div className="snake-dot"></div>
+      {props.snakeDot.map((dot, i) => {
+        const style = {
+          left:`${dot[0]}%`,
+          top:`${dot[1]}%`
+        };
+        <div className="snake-dot" ref= {ref} key={i} style={style}></div>
+      })}
     </div>
-  )
-}
+  );
+};
